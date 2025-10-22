d7c88f4aba83a6e5b00885d4ef7f1668681d5d53
Vikash pandit <vikash.pandit0444@gmail.com>
2025-10-22 18:34:49 +0530

    Prepare for Vercel deployment

--- api/server.js
+++ api/server.js
@@ -86,6 +86,7 @@ app.post('/api/chat',async (req,res) => {
 })
 
 
-app.listen(3000, () => {
-  console.log(`Server is running on port http://localhost:${port}`);
-});
+// app.listen(3000, () => {
+//   console.log(`Server is running on port http://localhost:${port}`);
+// });
+export default app;
